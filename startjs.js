e35f12fa9e6a2cf96a8e4d820069fc61e8342185
cl222pw <cl222pw@student.lnu.se>
2014-02-25 19:37:07 +0100

    Formulär på Start
    Lade till ett "fake" formulär längst ner på startsidan

--- startjs.js
+++ startjs.js
@@ -36,7 +36,7 @@ bildspel();
 
 /////////////////////////////////////////////////////////////////////
 
-//Startsida Toggle Knapp Kod//
+//Startsida Toggle Knapp 1//
 
 function toggle(){
 	//om showhide har class = hide, ändra den till class = show
@@ -55,40 +55,95 @@ hidebutton.onclick = toggle
 
 /////////////////////////////////////////////////////////////////////
 
-//Expanding Boxes Toggle
+//Startsida Toggle Knapp 2//
 
-function expand(element){
-	var target = document.getElementById(element);
-	var h = target.offsetHeight;
-	var sh = target.scrollHeight;
-	var loopTimer = setTimeout('expand(\''+element+'\')',8);
-	if(h < sh){
-		h += 5;
-	} else {
-		clearTimeout(loopTimer);
-	}
-	target.style.height = h+"px";
-}
-function retract(element){
-	var target = document.getElementById(element);
-	var h = target.offsetHeight;
-	var loopTimer = setTimeout('retract(\''+element+'\')',8);
-	if(h > 0){
-		h -= 5;
-	} else {
-		target.style.height = "0px";
-		clearTimeout(loopTimer);
-	}
-	target.style.height = h+"px";
+function ftoggle(){
+	//om showhide har class = hide, ändra den till class = show
+	if(document.getElementById("fshowhide").className == "hide")
+		document.getElementById("fshowhide").className = "show";
+	//annars ändra till class = hide
+	else
+		document.getElementById("fshowhide").className = "hide";
 }
 
+//Ger "hidebutton" alltså knappen man trycker på för att toggla ett namn
+var fhidebutton = document.querySelector("#fhidebutton");
+//Gör att när man trycker på showhide knappen så kör man toggle funktionen
+fhidebutton.onclick = ftoggle
+
+
+/////////////////////////////////////////////////////////////////////
+
+// Submit-knapp //
 
 
 
+function validate() {
+	//skapar "fail" som ger ett felmeddelande
+    var fail = 'You need to enter an e-mail!'
+	
+	//letar upp tbody från html-koden för att sedan lägga till tabellrader i den
+    var failtable = document.querySelector('#stbody');
+	//skapar en tabellrad som ska läggas in i tabellens body
+	var failtr = document.createElement('tr');
+	//skapar ett element som används senare
+	var failTd = document.createElement('td');
+    
+	//letar upp email idt från html-koden
+	var email = document.querySelector("#email");
+	
+	//skapar emailNum som blir NaN om email inte är siffor
+    var emailNum = parseFloat(email.value);
+
+	//om email är större än 0 OCH är ett nummer blir dens class "correct"
+	if(email.value.length > 0) {
+		button.className = 'correct';
+	}
+	//annars blir dens class "fail"
+	else {
+		button.className = 'fail';
+	}
+	//om klassen fail finns så skapar den en text-node som lägger till ett felmeddelande i tabellen
+	if (document.querySelector('.fail')) {
+		var failTextNode = document.createTextNode(fail);
+        failTd.appendChild(failTextNode)
+	    failtr.appendChild(failTd);
+		
+	    failtable.insertBefore(failtr, failtable.firstChild);
+		  
+	}
+	//om inte så körs funktionen submit
+	else {
+	submit(email.value);
+	}    
+}
+
+//funktionen submit
+function submit(email) {
+
+	//letar upp tbody från html-koden för att sedan lägga till tabellrader i den
+	var table = document.querySelector('#stbody');
+	//skapar en tabellrad som ska läggas in i tabellens body
+	var tr = document.createElement('tr');
+	//skapar ett element som används senare
+	var amountTd = document.createElement('td');
+    
+	//texten som ska visas senare
+    var text = ' A confirmation e-mail has been sent to ' + email + '!'
+    
+	//lägger till "text" variabeln som text i tabellen
+    var amountTextNode = document.createTextNode(text);
+     
+	amountTd.appendChild(amountTextNode)
+	tr.appendChild(amountTd);
+	table.insertBefore(tr, table.firstChild);
+}
 
 
 
+var button = document.querySelector("#sendbutton");
 
+button.onclick = validate;
 
 
 
